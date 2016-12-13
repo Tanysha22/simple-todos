050b07cf806b093ed5deb23c39d66bf1b5c3f1f9
Tanya <KuznetsovaTanya21@yandex.ru>
2016-12-13 18:38:05 +0300

    Adding user accounts

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
 
@@ -20,6 +21,9 @@ Template.body.helpers({
     // Otherwise, return all of the tasks
     return Tasks.find({}, { sort: { createdAt: -1 } });
   },
+  incompleteCount() {
+   return Tasks.find({ checked: { $ne: true } }).count();
+  },
 });
  
 Template.body.events({
@@ -35,6 +39,8 @@ Template.body.events({
     Tasks.insert({
       text,
       createdAt: new Date(), // current time
+	  owner: Meteor.userId(),
+      username: Meteor.user().username,
     });
  
     // Clear form
